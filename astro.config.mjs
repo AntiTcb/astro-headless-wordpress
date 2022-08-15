c88f22eabdd2fa4a405a26296dbe43014dae53aa
Alex Gravely <tcbskater@hotmail.com>
2022-08-15 12:40:06 -0400

    Start ygorg-astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
-
 import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind()],
-  site: `http://astro.build`,
+    integrations: [preact(), tailwind(), svelte()],
+    site: `https://ygorganization.com`
 });
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import { request } from 'graphql-request';
 
 export const queryWordPress = async <T>(query: string) => {
-  const uri = "http://headlesswpplayground.local/graphql";
+  const uri = "https://ygorganization.com/graphql";
   const results: T = await request(uri, query);
   return results;
 }
