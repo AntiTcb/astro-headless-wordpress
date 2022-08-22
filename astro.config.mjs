7ef1e91263bce359f5f1733e77cc0125a16bf330
Alex Gravely <tcbskater@hotmail.com>
2022-08-22 09:14:34 -0400

    Fix link

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-import vercel from '@astrojs/vercel'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
