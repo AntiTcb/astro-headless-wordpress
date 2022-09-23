dc372038b9d14e552be00f531cd9b08febee8767
Alex Gravely <tcbskater@hotmail.com>
2022-09-23 12:18:42 -0400

    code

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel/serverless';
+
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [preact(), tailwind(), svelte()],
-    site: `https://ygorganization.com`,
-    output: 'server',
-    adapter: vercel()
+  integrations: [preact(), tailwind(), svelte()],
+  site: `https://ygorganization.com`,
+  output: 'server',
+  adapter: vercel()
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
+    content: [
+        './src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}',
+        './node_modules/@brainandbones/skeleton/**/*.{html,js,svelte,ts}'
+    ],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/line-clamp'),
+        require('@brainandbones/skeleton/tailwind.cjs')
+    ],
 }
